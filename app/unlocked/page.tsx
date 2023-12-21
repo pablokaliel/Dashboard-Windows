d0fc87620b4e45b1cf3a14e68b8e8bba208e0388
Pablo Kaliel <pablo.akyell.441@gmail.com>
2023-12-21 16:05:59 -0300

    added itens the button windows and added icons in the screen

--- app/unlocked/page.tsx
+++ app/unlocked/page.tsx
@@ -14,125 +14,69 @@ import battery from "../../public/icons/Battery.svg";
 import overflow from "../../public/icons/Overflow.svg";
 import { useEffect, useState } from "react";
 
+import github from "../../public/icons/tasks/github.svg";
+import lixeira from "../../public/icons/tasks/Icon.png";
+import folder from "../../public/icons/tasks/Icon.svg";
+import notepad from "../../public/icons/tasks/Icon 2.svg";
+import paint from "../../public/icons/tasks/Icon 3.svg";
+import calculator from "../../public/icons/tasks/Icon 4.svg";
+import spotify from "../../public/icons/tasks/Icon 5.svg";
+import settings from "../../public/icons/tasks/Icon 6.svg";
+import mail from "../../public/icons/tasks/Icon 7.svg";
+import photos from "../../public/icons/tasks/Icon 8.svg";
+import xbox from "../../public/icons/tasks/Icon 9.svg";
+import pdf from "../../public/icons/tasks/Icon 10.svg";
+import word from "../../public/icons/tasks/Icon 11.svg";
+import excel from "../../public/icons/tasks/Icon 12.svg";
+import store from "../../public/icons/tasks/Icon 13.svg";
+import calendary from "../../public/icons/tasks/Icon 14.svg";
+import camera from "../../public/icons/tasks/Icon 15.svg";
+import steam from "../../public/icons/tasks/steam.svg";
+import chrome from "../../public/icons/tasks/chrome.svg";
+import discord from "../../public/icons/tasks/discord.svg";
+
 import { format } from "date-fns";
 import axios from "axios";
+
 type ModalContent = {
   [key: string]: JSX.Element | undefined;
 };
 
+type Button = {
+  src: string;
+  alt: string;
+  name: string;
+};
+
 const buttonsModal = {
   windows: [
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    { src: windows, alt: "clima", name: "item" },
-    // Adicione mais objetos conforme necessário
+    { src: folder, alt: "folder", name: "Folder" },
+    { src: notepad, alt: "bloco de notas", name: "Notepad" },
+    { src: paint, alt: "paint", name: "Paint" },
+    { src: calculator, alt: "calculadora", name: "Calculator" },
+    { src: spotify, alt: "spotify", name: "Spotify" },
+    { src: settings, alt: "configuracao", name: "Settings" },
+    { src: mail, alt: "email", name: "Mail" },
+    { src: photos, alt: "fotos", name: "Photo" },
+    { src: xbox, alt: "xbox", name: "Xbox" },
+    { src: pdf, alt: "pdf", name: "PDF" },
+    { src: word, alt: "word", name: "Word" },
+    { src: excel, alt: "excel", name: "Excel" },
+    { src: store, alt: "store", name: "Microsoft Store" },
+    { src: calendary, alt: "calendario", name: "Calendar" },
+    { src: camera, alt: "camera", name: "Camera" },
   ],
-  // Adicione mais plataformas ou tipos de botões conforme necessário
 };
 
 const RecommendedModal = {
   windows: [
-    { src: windows, title: "Clima", subtitle: "Item 1" },
-    { src: windows, title: "Clima", subtitle: "Item 2" },
-    { src: windows, title: "Clima", subtitle: "Item 3" },
-    { src: windows, title: "Clima", subtitle: "Item 4" },
-    { src: windows, title: "Clima", subtitle: "Item 5" },
-    { src: windows, title: "Clima", subtitle: "Item 6" },
-    // Adicione mais objetos conforme necessário
+    { src: notepad, title: "NotePad", subtitle: "2h atrás" },
+    { src: spotify, title: "Spotify", subtitle: "17m atrás" },
+    { src: folder, title: "Meus projetos", subtitle: "Ontem às 13h15" },
+    { src: discord, title: "Discord", subtitle: "Ontem às 16h20" },
+    { src: suun, title: "Clima", subtitle: "5h atrás" },
+    { src: mail, title: "Email", subtitle: "12h atrás" },
   ],
-  // Adicione mais plataformas ou tipos de botões conforme necessário
-};
-
-const modalContents: ModalContent = {
-  windows: (
-    <div className="flex flex-col gap-4">
-      <div className="flex justify-between">
-        <h2 className="text-[17px] font-bold">Pinned</h2>
-        <button className="bg-white/10 text-sm px-2 rounded">All Apps</button>
-      </div>
-      <div className="grid flex-1 grid-cols-6 ">
-        {buttonsModal.windows.map((button, index) => (
-          <button
-            key={index}
-            className="flex flex-col mb-4 items-center justify-center"
-          >
-            <Image src={button.src} alt={button.alt} />
-            <p className="text-sm font-normal">{button.name}</p>
-          </button>
-        ))}
-      </div>
-      <div className="flex justify-between">
-        <h2 className="text-[17px] font-bold">Recommended</h2>
-        <button className="bg-white/10 text-sm px-2 rounded">More</button>
-      </div>
-      <div className="grid grid-cols-2 gap-4">
-        {/* Itens recomendados */}
-        {RecommendedModal.windows.map((item, index) => (
-          <div
-            key={index}
-            className="flex px-10 items-center justify-start gap-4"
-          >
-            <Image src={item.src} alt={item.title} />
-            <button className="flex flex-col items-center justify-center">
-              <p className="text-sm">{item.title}</p>
-              <p className="text-xs text-gray-400">{item.subtitle}</p>
-            </button>
-          </div>
-        ))}
-      </div>
-    </div>
-  ),
-  explorer: (
-    <div>
-      <h2>Conteúdo específico para Explorer</h2>
-      <p>Aqui está o conteúdo para o Explorer...</p>
-      {/* Adicione outros elementos HTML conforme necessário */}
-    </div>
-  ),
-  search: (
-    <div>
-      <h2>Conteúdo específico para Search</h2>
-      <p>Aqui está o conteúdo para o Search...</p>
-      {/* Adicione outros elementos HTML conforme necessário */}
-    </div>
-  ),
-  desktopmaginer: (
-    <div>
-      <h2>Conteúdo específico para Desktop Magner</h2>
-      <p>Aqui está o conteúdo para o Desktop Magner...</p>
-      {/* Adicione outros elementos HTML conforme necessário */}
-    </div>
-  ),
-  chat: (
-    <div>
-      <h2>Conteúdo específico para Chat</h2>
-      <p>Aqui está o conteúdo para o Chat...</p>
-      {/* Adicione outros elementos HTML conforme necessário */}
-    </div>
-  ),
-  microsoft: (
-    <div>
-      <h2>Conteúdo específico para Microsoft</h2>
-      <p>Aqui está o conteúdo para o Microsoft...</p>
-      {/* Adicione outros elementos HTML conforme necessário */}
-    </div>
-  ),
-  // Adicione outros tipos de conteúdo conforme necessário
 };
 
 export default function Page() {
@@ -142,6 +86,98 @@ export default function Page() {
   const [date, setDate] = useState("");
   const [weather, setWeather] = useState<any>(null);
   const [, setLocationAccess] = useState(false);
+
+  const [searchText, setSearchText] = useState<string>("");
+  const [filteredButtons, setFilteredButtons] = useState<Button[]>([]);
+  const modalContents: ModalContent = {
+    windows: (
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-between">
+          <h2 className="text-[17px] font-bold">Pinned</h2>
+          <button className="bg-white/10 text-sm px-2 rounded">All Apps</button>
+        </div>
+        <div className="grid flex-1 grid-cols-6 ">
+          {filteredButtons.length > 0 ? (
+            filteredButtons.map((button: Button, index: number) => (
+              <button
+                key={index}
+                className="flex flex-col w-auto mb-4 items-center justify-center hover:bg-black/20 "
+              >
+                <Image
+                  src={button.src}
+                  alt={button.alt}
+                  height={30}
+                  width={30}
+                />
+                <p className="text-sm mt-2 font-normal">{button.name}</p>
+              </button>
+            ))
+          ) : (
+            <p className="text-center text-red-500">Não encontrado</p>
+          )}
+        </div>
+        <div className="flex justify-between">
+          <h2 className="text-[17px] font-bold">Recommended</h2>
+          <button className="bg-white/10 text-sm px-2 rounded">More</button>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          {RecommendedModal.windows.map((item, index) => (
+            <button
+              key={index}
+              className="flex pr-10 items-center justify-start gap-4"
+            >
+              <Image src={item.src} alt={item.title} width={30} height={30} />
+              <div className="flex flex-col items-start justify-center">
+                <p className="text-sm">{item.title}</p>
+                <p className="text-xs text-gray-400">{item.subtitle}</p>
+              </div>
+            </button>
+          ))}
+        </div>
+      </div>
+    ),
+    explorer: (
+      <div>
+        <h2>Conteúdo específico para Explorer</h2>
+        <p>Aqui está o conteúdo para o Explorer...</p>
+      </div>
+    ),
+    search: (
+      <div>
+        <h2>Conteúdo específico para Search</h2>
+        <p>Aqui está o conteúdo para o Search...</p>
+      </div>
+    ),
+    desktopmaginer: (
+      <div>
+        <h2>Conteúdo específico para Desktop Magner</h2>
+        <p>Aqui está o conteúdo para o Desktop Magner...</p>
+      </div>
+    ),
+    chat: (
+      <div>
+        <h2>Conteúdo específico para Chat</h2>
+        <p>Aqui está o conteúdo para o Chat...</p>
+      </div>
+    ),
+    microsoft: (
+      <div>
+        <h2>Conteúdo específico para Microsoft</h2>
+        <p>Aqui está o conteúdo para o Microsoft...</p>
+      </div>
+    ),
+  };
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearchText(e.target.value);
+  };
+
+  useEffect(() => {
+    const filtered = buttonsModal.windows.filter((button) =>
+      button.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredButtons(filtered);
+  }, [searchText]);
 
   const [showModal, setShowModal] = useState(false);
   const [modalContent, setModalContent] = useState<string>("");
@@ -190,7 +226,6 @@ export default function Page() {
     };
 
     document.addEventListener("keydown", handleEscKey, false);
-
     return () => {
       document.removeEventListener("keydown", handleEscKey, false);
     };
@@ -219,16 +254,73 @@ export default function Page() {
 
   return (
     <main className="min-h-screen w-full">
-      <div className="flex flex-col w-full h-screen items-center justify-between">
-        <div className="mt-44 h-full flex flex-col items-center">
-          <div className="text-8xl bold">hora</div>
-          <span className="text-xl">data</span>
+      <div className="flex flex-col w-full h-screen ">
+        <div className="flex h-full justify-between">
+          <div className=" grid grid-cols-2 gap-2 h-fit">
+            <button className="px-2 pt-5 w-auto flex flex-col items-center justify-center hover:bg-white/30 duration-200">
+              <div>
+                <Image src={notepad} alt="clima" width={30} height={30} />
+              </div>
+              <span className="text-sm mt-2">NotePad</span>
+            </button>
+
+            <button className="px-2 pt-5 w-auto flex flex-col items-center justify-center hover:bg-white/30 duration-200">
+              <div>
+                <Image src={folder} alt="clima" width={30} height={30} />
+              </div>
+              <span className="text-sm mt-2">Meus projetos</span>
+            </button>
+
+            <button className="px-2 pt-5 w-auto flex flex-col items-center justify-center hover:bg-white/30 duration-200">
+              <div>
+                <Image src={spotify} alt="clima" width={30} height={30} />
+              </div>
+              <span className="text-sm mt-2">Spotify</span>
+            </button>
+
+            <button className="px-2 pt-5 w-auto flex flex-col items-center justify-center hover:bg-white/30 duration-200">
+              <div>
+                <Image src={github} alt="clima" width={30} height={30} />
+              </div>
+              <span className="text-sm mt-2">GitHub</span>
+            </button>
+          </div>
+
+          <div className=" h-full flex flex-col ">
+            <button className="px-5 pt-5 w-auto flex flex-col items-center justify-center hover:bg-white/30 ">
+              <div>
+                <Image src={lixeira} alt="clima" width={30} height={30} />
+              </div>
+              <span className="text-sm mt-2">Lixeira</span>
+            </button>
+
+            <button className="px-5 pt-5 w-auto flex flex-col items-center justify-center hover:bg-white/30 mt-2">
+              <div>
+                <Image src={steam} alt="clima" width={30} height={30} />
+              </div>
+              <span className="text-sm mt-2">Steam</span>
+            </button>
+
+            <button className="px-5 pt-5 w-auto flex flex-col items-center justify-center hover:bg-white/30 mt-2">
+              <div>
+                <Image src={discord} alt="clima" width={30} height={30} />
+              </div>
+              <span className="text-sm mt-2">Discord</span>
+            </button>
+
+            <button className="px-5 pt-5 w-auto flex flex-col items-center justify-center hover:bg-white/30 mt-2">
+              <div>
+                <Image src={chrome} alt="clima" width={30} height={30} />
+              </div>
+              <span className="text-sm mt-2">Chrome</span>
+            </button>
+          </div>
         </div>
 
         <footer className="flex justify-between bg-[#444444]/30 backdrop-blur-xl items-center h-[60px] w-full">
-          <div className="ml-4 items-center h-[50px]  flex gap-3">
+          <div className="ml-4 items-center h-[50px] flex gap-3">
             <Image src={suun} alt="clima" />
-            <div className="">
+            <div>
               {weather && weather.main && (
                 <>
                   <p className="text-xs font-bold">
@@ -242,22 +334,27 @@ export default function Page() {
 
           <div className="flex flex-1 items-center justify-center gap-7">
             <button onClick={() => toggleModalWithContent("windows")}>
-              <Image src={windows} alt="clima" />
+              <Image height={24} width={24} src={windows} alt="clima" />
             </button>
+
             <button onClick={() => toggleModalWithContent("search")}>
-              <Image src={search} alt="clima" />
+              <Image height={24} width={24} src={search} alt="clima" />
             </button>
+
             <button onClick={() => toggleModalWithContent("desktopmaginer")}>
-              <Image src={maniger} alt="clima" />
+              <Image height={24} width={24} src={maniger} alt="clima" />
             </button>
+
             <button onClick={() => toggleModalWithContent("chat")}>
-              <Image src={chat} alt="clima" />
+              <Image height={24} width={24} src={chat} alt="clima" />
             </button>
+
             <button onClick={() => toggleModalWithContent("explorer")}>
-              <Image src={explorer} alt="clima" />
+              <Image height={24} width={24} src={explorer} alt="clima" />
             </button>
+
             <button onClick={() => toggleModalWithContent("microsoft")}>
-              <Image src={microsoft} alt="clima" />
+              <Image height={24} width={24} src={microsoft} alt="clima" />
             </button>
           </div>
 
@@ -276,37 +373,47 @@ export default function Page() {
           </div>
         </footer>
       </div>
-      {showModal && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
-          <div className="relative mx-auto w-full h-full flex items-center justify-center">
-            <div className="bg-[#212121]/60 flex flex-col gap-10 backdrop-blur-xl w-[700px] rounded shadow-lg">
-              <div className="flex justify-between items-center px-10 pr-10 pt-10">
-                <div className="h-[45px]  w-full bg-[#1e1e1e]/70">
-                  <input
-                    className="w-full px-[55px] h-full bg-transparent"
-                    placeholder="type here to search"
-                  />
-                </div>
+
+      <div
+        onClick={toggleModal}
+        className={`fixed ${
+          showModal ? "translate-y-0" : "translate-y-full"
+        } transition-transform duration-300 inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none`}
+      >
+        <div className="relative mx-auto w-full h-full flex items-center justify-center">
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className="bg-[#212121]/60 border-white border-[1.4px] border-opacity-20 flex flex-col gap-10 backdrop-blur-xl w-[700px] rounded shadow-lg"
+          >
+            <div className="flex justify-between items-center px-10 pr-10 pt-10">
+              <div className="h-[45px]  w-full bg-[#1e1e1e]/70">
+                <input
+                  className="w-full px-[55px] h-full bg-transparent"
+                  placeholder="type here to search"
+                  onChange={handleSearch}
+                />
               </div>
-              <div className="px-10">
-                <div className=" ">{modalContents[modalContent]}</div>
-              </div>
-              <div className="h-16 border-t-[1.4px] flex-col flex justify-center border-white border-opacity-20 px-[70px] bg-black/10">
-                <div className="flex gap-3 items-center ">
-                  <Image
-                    src="https://github.com/pablokaliel.png"
-                    className="rounded-full"
-                    alt=""
-                    width={40}
-                    height={40}
-                  />
-                  <p className="text-sm font-normal">name</p>
-                </div>
+            </div>
+
+            <div className="px-10">
+              <div className=" ">{modalContents[modalContent]}</div>
+            </div>
+
+            <div className="h-16 border-t-[1.4px] flex-col flex justify-center border-white border-opacity-20 px-[70px] bg-black/10">
+              <div className="flex gap-3 items-center ">
+                <Image
+                  src="https://github.com/pablokaliel.png"
+                  className="rounded-full"
+                  alt=""
+                  width={40}
+                  height={40}
+                />
+                <p className="text-sm font-normal">name</p>
               </div>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </main>
   );
 }
